359c9508d900b6c59e322c51d25691d8b98c0175
Lucas Sousa Silva <lucassousasilva@yahoo.com>
2024-08-13 13:04:47 -0300

    Some modifications

--- sources/ms-catalog/app/src/test/java/com/microshop/api/AppTest.java
+++ sources/ms-catalog/app/src/test/java/com/microshop/api/AppTest.java
@@ -3,12 +3,16 @@
  */
 package com.microshop.api;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    @Test void contextLoads() {
+        assertEquals(1, 1);
     }
 }
+
