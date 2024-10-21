57f22a7763e8e56d777a8be98d0d584db9c3f46e
Lucas Sousa Silva <lucassousasilva@yahoo.com>
2024-10-21 13:52:43 -0300

    chore: fixed something

--- sources/ms-image_store/app/src/main/java/com/microshop/image_store/controller/ImageController.java
+++ sources/ms-image_store/app/src/main/java/com/microshop/image_store/controller/ImageController.java
@@ -1,33 +1,25 @@
 package com.microshop.image_store.controller;
 
-import com.microshop.image_store.services.ImageUploader;
+import com.microshop.image_store.services.ImageService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import com.microshop.image_store.dto.SaveImageRequest;
 import com.microshop.image_store.images.ImageSpec;
@@ -36,33 +28,35 @@ import com.microshop.image_store.images.ImageSpec;
 @RequestMapping("/image-store")
 public class ImageController {
 
-    @Autowired private ImageUploader imageUploader;
+    @Autowired private ImageService imageService;
 
-    @PostMapping(path = "form",consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> uploadImage(
         @ModelAttribute SaveImageRequest saveImageRequest
     ) {
-        try {
-            var image_code = saveImageRequest.getCode();
-            var image_file = saveImageRequest.getImageFile();
-            if(image_file == null){
-                System.out.println("AAAAA, ImageFile is null");
+        Integer imageCode = saveImageRequest.getCode();
+        MultipartFile imageFile = saveImageRequest.getImageFile();
+        if(imageFile != null){
+            try{
+                imageService.uploadJPEGImage(imageCode, imageFile);
+            }catch(IOException e){
                 return ResponseEntity.status(500).build();
             }
-            imageUploader.uploadJPEGImage(image_code, image_file.getBytes());
             return ResponseEntity.ok(null);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(500).build();
         }
+
+        // FATAL failure ImageFIle cannot be empty!
+        System.out.println("AAAAA, ImageFile is null");
+        return ResponseEntity.status(500).build();
     }
+
 
     @GetMapping("/{image_code}")
     public ResponseEntity<Resource> getImage(
         @RequestBody ImageSpec spec,
         @PathVariable(name = "image_code") int image_code
     ){
-        ResponseInputStream<GetObjectResponse> ris = imageUploader.downloadJPEGImage(image_code, spec);
+        ResponseInputStream<GetObjectResponse> ris = imageService.downloadJPEGImage(image_code, spec);
         InputStreamResource resource = new InputStreamResource(ris);
         try{
             var response = ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).contentLength(resource.contentLength()).body((Resource)resource);
--- sources/ms-image_store/app/src/main/java/com/microshop/image_store/services/ImageService.java
+++ sources/ms-image_store/app/src/main/java/com/microshop/image_store/services/ImageService.java
@@ -2,8 +2,8 @@ package com.microshop.image_store.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
@@ -15,43 +15,62 @@ import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import com.microshop.image_store.images.ImageSpec;
 
+import java.io.IOException;
+import java.io.InputStream;
 @Service
-public class ImageUploader {
-    private final String bucketName = "microshop.product-images";
+public class ImageService {
+    private static final String bucketName = "microshop.product-images";
     private final S3Client s3Client;
 
+    private static final String JPEG_CONTENT_TYPE = "image/jpeg";
+
     @Autowired
-    public ImageUploader(S3Client s3Client) {
+    public ImageService(S3Client s3Client) {
         this.s3Client = s3Client;
     }
-
-    /**
-     * Uploads an JPEG image to the S3
-     *
-     * @param image_code - is the code of the image
-     * @param image_bytes - is the binary of the image (must be jpeg)
-     * @return
-     */
-    public void uploadJPEGImage(Integer image_code, byte[] image_bytes) {
-
-        var JPEG_CONTENT_TYPE = "image/jpeg";
+    
+    public void uploadJPEGImage(Integer imageCode, MultipartFile imageFile) throws IOException{
         PutObjectRequest por =
                 PutObjectRequest.builder()
                         .bucket(bucketName)
-                        .key(image_code.toString())
+                        .key(imageCode.toString())
                         .contentType(JPEG_CONTENT_TYPE)
                         .build();
+        InputStream imageFileInputStream = imageFile.getInputStream();
+        RequestBody rb = RequestBody.fromInputStream(imageFileInputStream, imageFile.getSize());
+        s3Client.putObject(por, rb);
+    }
 
-        PutObjectResponse response = s3Client.putObject(por, RequestBody.fromBytes(image_bytes));
+    /**
+     * Uploads an JPEG image to the S3.
+     * It's an idepotent operation, if such file with image_code already
+     * exists, then it'll be overriden !!
+     *
+     * @param image_code - is the code of the image
+     * @param image_bytes - is the binary of the image (must be jpeg)
+     */
+    public void uploadJPEGImage(Integer imageCode, InputStream imageInputStream, Long length) {
+
+        PutObjectRequest por =
+                PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(imageCode.toString())
+                        .contentType(JPEG_CONTENT_TYPE)
+                        .build();
+        RequestBody rb = RequestBody.fromInputStream(imageInputStream, length);
+        s3Client.putObject(por, rb);
     }
 
     /**
      * Returns the desidered image from the given specification.
      */
     public ResponseInputStream<GetObjectResponse> downloadJPEGImage(Integer image_code, ImageSpec spec){
-        var JPEG_CONTENT_TYPE = "image/jpeg";
         GetObjectRequest gor = GetObjectRequest.builder().bucket(bucketName).key(image_code.toString()).responseContentType(JPEG_CONTENT_TYPE).build();
         ResponseInputStream<GetObjectResponse> response = s3Client.getObject(gor, ResponseTransformer.toInputStream());
         return response;
     }
+
+    /**
+     *
+     */
 }
