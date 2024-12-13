26d9e769edcb77ef21fd6b0135735e85e0ccaee7
Lucas Sousa Silva <lucassousasilva@yahoo.com>
2024-12-12 22:07:05 -0300

    Added more tests

--- sources/ms-backend/app/src/main/java/com/microshop/dto/CategoryDTO.java
+++ sources/ms-backend/app/src/main/java/com/microshop/dto/CategoryDTO.java
@@ -2,9 +2,11 @@ package com.microshop.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDTO {
     private String id;
     private String name;
--- sources/ms-backend/app/src/test/java/com/microshop/controller/ProductControllerTest.java
+++ sources/ms-backend/app/src/test/java/com/microshop/controller/ProductControllerTest.java
@@ -1,24 +1,30 @@
 package com.microshop.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.net.URI;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(ProductController.class)
+// @WebMvcTest(ProductController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProductControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
-    void shouldPersistAProduct() throws Exception {
+    void shouldPersistAProduct(@Autowired MockMvc mvc) throws Exception {
+        String newCatJson = "{\"name\":\"Canetas\", \"path\":\"/canetas\"}";
+        String expectedResultJson = "{\"id\":1,\"name\":\"Canetas\", \"path\":\"/canetas\"}";
+        mvc.perform(post("/products").content(newCatJson.getBytes("UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResultJson));
         // NewProductDTO product = new NewProductDTO("Celulares", "/celulares");
+        // mockMvc.perform(post(URI.create("/products")).content("{\"name\":\"Celulares\", \"path\":\"/celulares\"}"))
+        //        .andExpect(status().isOk());
 
-        mockMvc.perform(post(URI.create("/products")).content("{\"name\":\"Celulares\", \"path\":\"/celulares\"}"))
-                .andExpect(status().isOk());
     }
 }
