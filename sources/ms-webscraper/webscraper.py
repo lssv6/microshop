6b2201ce35190341ed251a0b3d2fa4e19a187184
lssv6 <lucassousasilva@yahoo.com>
2025-03-13 16:38:39 -0300

    fix: fixed errors shown by ruff

--- sources/ms-webscraper/webscraper.py
+++ sources/ms-webscraper/webscraper.py
@@ -8,15 +8,17 @@ from urllib.parse import urlparse, parse_qs
 SITEMAP_URL = "https://www.kabum.com.br/sitemap.xml"
 rootWasCrawled = False
 
+
 async def sitemap_worker(queue: asyncio.Queue, name: str):
     global rootWasCrawled
     global SITEMAP_URL
+
     def get_locs(xml: BeautifulSoup):
         url_locs = xml.find_all("loc")
         for element in url_locs:
             yield element.text
 
-    async def enqueue_urls(queue:asyncio.Queue, *urls: str):
+    async def enqueue_urls(queue: asyncio.Queue, *urls: str):
         for url in urls:
             await queue.put(url)
 
@@ -43,7 +45,7 @@ async def sitemap_worker(queue: asyncio.Queue, name: str):
                 # Parse XML file
                 xml = BeautifulSoup(text, "lxml-xml")
 
-                # If there is a sitemap, crawl as sitemapindex 
+                # If there is a sitemap, crawl as sitemapindex
                 if xml.find("sitemap"):
                     locs = get_locs(xml)
                     await enqueue_urls(queue, *locs)
@@ -56,12 +58,12 @@ async def sitemap_worker(queue: asyncio.Queue, name: str):
             queue.task_done()
 
 
-
-def write_category_data(category_path:str, page_number:int, category_data: dict):
+def write_category_data(category_path: str, page_number: int, category_data: dict):
     with open(f"{category_path}/{page_number}.json", "w") as file:
         json.dump(category_data, file)
 
-async def category_worker(queue:asyncio.Queue, name: str):
+
+async def category_worker(queue: asyncio.Queue, name: str):
     async with aiohttp.ClientSession() as session:
         while True:
             url = await queue.get()
@@ -71,16 +73,15 @@ async def category_worker(queue:asyncio.Queue, name: str):
 
                 text = await response.text()
                 print(f"{name} --> {url=} downloaded")
-                
 
                 # Here comes the lambança
                 html = BeautifulSoup(text, "lxml")
-                script_element = html.find(attrs={"id":"__NEXT_DATA__"})
+                script_element = html.find(attrs={"id": "__NEXT_DATA__"})
 
                 if script_element is None:
                     queue.task_done()
                     continue
-                
+
                 # Trickiest part of the code.
                 # HACK: this is a part of the code can be broken due to kabum changes.
                 # It's related to mining the data from the page via nasty methods.
@@ -111,12 +112,24 @@ async def category_worker(queue:asyncio.Queue, name: str):
                 if current_page < totalPagesCount:
                     # Perhaps _replace was supposed to be a private method.
                     # Using anyway
-                    next_page_url = parsed_url._replace("query", f"page_number={page_number+1}").geturl()
+                    next_page_url = parsed_url._replace(
+                        "query", f"page_number={page_number + 1}"
+                    ).geturl()
                     await queue.put(next_page_url)
             queue.task_done()
 
 
 async def main():
+    def is_product_url(url):
+        return "/produto/" in url
+
+    def is_category_url(url):
+        return (
+            (not is_product_url(url))
+            and url not in BLACKLIST
+            and all(map(lambda start: not url.startswith(start), START_BLACKLIST))
+        )
+
     queue: asyncio.Queue = asyncio.Queue()
     await queue.put(SITEMAP_URL)
 
@@ -126,9 +139,9 @@ async def main():
     ]
 
     url_list = []
-    for l in await asyncio.gather(*workers):
-        url_list.extend(l)
-    
+    for li in await asyncio.gather(*workers):
+        url_list.extend(li)
+
     BLACKLIST = [
         "https://www.kabum.com.br",
         "https://www.kabum.com.br/login",
@@ -147,32 +160,26 @@ async def main():
         "https://www.kabum.com.br/busca/",
     ]
 
-    is_product_url = lambda url : "/produto/" in url
-    is_category_url = lambda url : (not is_product_url(url)) and url not in BLACKLIST and all(map(lambda start: not url.startswith(start),START_BLACKLIST))
-
     product_list = [*filter(is_product_url, url_list)]
-    category_list= [*filter(is_category_url, url_list)]
+    category_list = [*filter(is_category_url, url_list)]
 
     print(f"Found {len(product_list)} products.")
     print(f"Found {len(category_list)} categories.")
 
-    print(f"Trying to crawl deeply")
+    print("Trying to crawl deeply")
 
     workers = [
         asyncio.create_task(category_worker(queue, f"cworker-{i}"), name=f"cworker-{i}")
         for i in range(128)
-
     ]
 
     for category in category_list:
         await queue.put(category)
     await queue.join()
-    
+
     for w in workers:
         w.cancel()
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
