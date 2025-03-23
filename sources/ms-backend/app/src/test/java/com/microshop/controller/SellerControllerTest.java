0f0fb648ddb76cb53b4153862d30d94902b89830
lssv6 <lucassousasilva@yahoo.com>
2025-03-23 15:56:17 -0300

    feat: seller and seller tests

--- sources/ms-backend/app/src/test/java/com/microshop/controller/SellerControllerTest.java
+++ sources/ms-backend/app/src/test/java/com/microshop/controller/SellerControllerTest.java
@@ -4,28 +4,26 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Optional;
+import com.microshop.dto.SellerDTO;
+import com.microshop.service.SellerService;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.microshop.dto.SellerDTO;
-import com.microshop.service.SellerService;
+import java.util.Optional;
 
 @WebMvcTest(controllers = SellerController.class)
-public class SellerControllerTest{
-    @MockitoBean
-    private SellerService sellerService;
+public class SellerControllerTest {
+    @MockitoBean private SellerService sellerService;
 
-    @Autowired
-    private MockMvc mvc;
+    @Autowired private MockMvc mvc;
 
     @Test
-    void testFindById() throws Exception{
+    void testFindById() throws Exception {
         SellerDTO seller = new SellerDTO();
         seller.setId(8L);
         seller.setName("Jooj");
@@ -34,23 +32,20 @@ public class SellerControllerTest{
         Mockito.when(sellerService.findById(8L)).thenReturn(Optional.of(seller));
 
         // When - Then
-        mvc.perform(
-                get("/seller/{id}", 8L)
-        ).andExpect(status().isOk())
-        .andExpect(jsonPath("$.id").value(8L));
+        mvc.perform(get("/seller/{id}", 8L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(8L));
     }
+
     @Test
-    void shouldNotFindByWrongId() throws Exception{
-        SellerDTO seller = new SellerDTO();
-        seller.setId(8L);
-        seller.setName("Jooj");
-        seller.setVersion(999L);
+    void testUnableToFind() throws Exception {
         // Given
-        Mockito.when(sellerService.findById(8L)).thenReturn(Optional.of(seller));
+        Mockito.when(sellerService.findById(10L)).thenReturn(Optional.empty());
 
         // When - Then
         mvc.perform(
-                get("/seller/{id}", 10L) // Wrong Id
-        ).andExpect(status().is4xxClientError());
+                        get("/seller/{id}", 10L) // Wrong Id
+                        )
+                .andExpect(status().is4xxClientError());
     }
 }
