d830eac39605cfb1d3b2b5f9c3a0dbd7d183e92c
lssv6 <lucassousasilva@yahoo.com>
2025-04-15 19:04:20 -0300

    feat: added breadcrumbs(deactivated biome-check)

--- sources/ms-frontend/src/App.vue
+++ sources/ms-frontend/src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import BreadcrumbBar from "./components/BreadcrumbBar.vue";
+import CategoryHeader from "./components/CategoryHeader.vue";
 import HeaderComponent from "./components/HeaderComponent.vue";
 </script>
 
@@ -7,7 +9,9 @@ import HeaderComponent from "./components/HeaderComponent.vue";
   <header>
       <HeaderComponent></HeaderComponent>
   </header>
-  <main>
+  <main class="centered">
+    <BreadcrumbBar></BreadcrumbBar>
+    <CategoryHeader></CategoryHeader>
     <h1>Here's how to code in Vue</h1>
     <h1>Here's how to code in Vue</h1>
     <h1>Here's how to code in Vue</h1>
---
+++ sources/ms-frontend/src/components/CategoryHeader.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const categoryName = "Processador AMD";
+</script>
+
+<template>
+    <div class="header-container bg-primary">
+      <h4 class="text-white fw-bold">{{categoryName}}</h4>
+    </div>
+</template>
