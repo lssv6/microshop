bf5d26c610849f736241f68c6b78fc4719d61944
lssv6 <lucassousasilva@yahoo.com>
2025-05-23 19:54:56 -0300

    deleted data importation. Next task is to create a separated project for this

--- sources/ms-backend/app/src/main/java/com/microshop/component/DataImport.java
+++
@@ -1,9 +0,0 @@
-package com.microshop.component;
-
-@Component
-public class DataImport implements CommandLineRunner {
-    @Override
-    public void run(String... args) {
-        // Do nothing... Bitchslap
-    }
-}
